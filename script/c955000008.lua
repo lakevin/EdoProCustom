98f30b1bacbc4740638397072f9803dd829251b7
lakevin <KevinLa95@web.de>
2023-03-04 19:13:14 +0100

    Bugfixing

--- script/c955000008.lua
+++ script/c955000008.lua
@@ -1,22 +1,23 @@
 -- Protectrix Spear
-function c955000008.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--1) special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c955000008.spcon)
-	e1:SetOperation(c955000008.spop)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--2) discard and to hand
 	e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCountLimit(1,955000008+EFFECT_COUNT_CODE_OATH)
-	e2:SetCost(c955000008.scost)
-	e2:SetTarget(c955000008.starget)
-	e2:SetOperation(c955000008.soperation)
+	e2:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e2:SetCost(s.scost)
+	e2:SetTarget(s.starget)
+	e2:SetOperation(s.soperation)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -26,57 +27,57 @@ function c955000008.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetCondition(c955000008.recon)
+	e4:SetCondition(s.recon)
 	e4:SetValue(LOCATION_REMOVED)
 	c:RegisterEffect(e4)
-	if not c955000008.global_check then
-		c955000008.global_check=true
+	if not s.global_check then
+		s.global_check=true
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_SUMMON_SUCCESS)
-		ge1:SetLabel(955000008)
+		ge1:SetLabel(id)
 		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		ge1:SetOperation(aux.sumreg)
 		Duel.RegisterEffect(ge1,0)
 		local ge2=ge1:Clone()
 		ge2:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge2:SetLabel(955000008)
+		ge2:SetLabel(id)
 		Duel.RegisterEffect(ge2,0)
 	end
 end
 
 --1)
-function c955000008.spfilter(c)
+function s.spfilter(c)
 	return c:IsSetCard(0x9990) and c:IsType(TYPE_TRAP) and c:IsAbleToRemoveAsCost()
 end
-function c955000008.spcon(e,c)
+function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c955000008.spfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil)
 end
-function c955000008.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c955000008.spfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 
 --2)
-function c955000008.filter(c)
+function s.filter(c)
 	return c:IsSetCard(0x9990) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
 end
-function c955000008.scost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.scost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
 end
-function c955000008.starget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(c955000008.filter,tp,LOCATION_DECK,0,1,nil) end
+function s.starget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c955000008.soperation(e,tp,eg,ep,ev,re,r,rp)
+function s.soperation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c955000008.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
@@ -84,6 +85,6 @@ function c955000008.soperation(e,tp,eg,ep,ev,re,r,rp)
 end
 
 --3)
-function c955000008.recon(e)
+function s.recon(e)
 	return e:GetHandler():IsFaceup()
 end
