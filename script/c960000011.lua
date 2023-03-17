b1ab822915329c56e9192a0cb1d1ec24b01b8a0e
kla <kevinla95@web.de>
2023-03-17 19:54:24 +0100

    Bugfixes

--- script/c960000011.lua
+++ script/c960000011.lua
@@ -2,12 +2,6 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--[[cannot special summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	c:RegisterEffect(e1)]]--
 	-- (1) base attack
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
