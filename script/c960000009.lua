3e4d26b29d3336965ed768331cc190babcdd1c2f
kla <kevinla95@web.de>
2023-03-18 15:17:47 +0100

    Hotfix Pandora

--- script/c960000009.lua
+++ script/c960000009.lua
@@ -1,113 +1,33 @@
--- Contract Zero
+-- Pact with the Black Winged Chain
 local s,id=GetID()
 function s.initial_effect(c)
-	-- 1) Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_END_PHASE+TIMING_MAIN_END)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	--Activate
+	local e1=Ritual.AddProcEqual{handler=c,filter=s.ritualfil,location=LOCATION_DECK|LOCATION_REMOVED,matfilter=s.mfilter,stage2=s.stage2}
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
-	-- 1) Activate
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetHintTiming(0,TIMING_END_PHASE+TIMING_MAIN_END)
-	e2:SetCondition(s.condition2)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
-	c:RegisterEffect(e2)
-	-- 1) Activate
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_ACTIVATE)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetHintTiming(0,TIMING_END_PHASE+TIMING_MAIN_END)
-	e3:SetCondition(s.condition3)
-	e3:SetTarget(s.target)
-	e3:SetOperation(s.operation)
-	c:RegisterEffect(e3)
-	-- 2) destroy
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e4:SetCode(EVENT_LEAVE_FIELD)
-	e4:SetOperation(s.desop)
-	c:RegisterEffect(e4)
 end
+s.listed_series={0x9998,0x9999}
 
--- 1)
-function s.cfilter1(c)
-	return c:IsFaceup() and c:IsCode(960000001)
+function s.mfilter(c)
+	return c:IsLocation(LOCATION_MZONE|LOCATION_GRAVE|LOCATION_REMOVED) and c:HasLevel() and c:IsSetCard(0x9999) and c:IsAbleToDeck()
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter1,tp,LOCATION_ONFIELD,0,1,nil)
-end
--- con2
-function s.cfilter2(c)
-	return c:IsFaceup() and c:IsCode(960000002)
-end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_ONFIELD,0,1,nil)
-end
--- con3
-function s.cfilter3(c)
-	return c:IsFaceup() and c:IsCode(960000003)
-end
-function s.condition3(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter3,tp,LOCATION_ONFIELD,0,1,nil)
-end
-
-function s.filter(c,e,tp)
-	return c:IsSetCard(0x9999) and c:GetLevel()==9 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+function s.stage2(mat,e,tp,eg,ep,ev,re,r,rp,tc)
+	tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,1))
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(s.sdescon)
-	e1:SetOperation(s.sdesop)
-	if Duel.GetCurrentPhase()==PHASE_END and Duel.GetTurnPlayer()~=tp then
-		e1:SetLabel(Duel.GetTurnCount())
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,2)
-	else
-		e1:SetLabel(0)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
-	end
-	e:GetHandler():RegisterEffect(e1)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetLabelObject(tc)
+	e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+	e1:SetCondition(s.tdcon)
+	e1:SetOperation(s.tdop)
+	Duel.RegisterEffect(e1,tp)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	local tc=g:GetFirst()
-	--if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-	--end
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(1-tp) and e:GetLabelObject():GetFlagEffect(id)>0
 end
-
--- 2)
-function s.rcon(e)
-	return e:GetOwner():IsHasCardTarget(e:GetHandler())
-end
-function s.sdescon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp and Duel.GetTurnCount()~=e:GetLabel()
-end
-function s.sdesop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetHandler():GetFirstCardTarget()
-	if tc and tc:IsLocation(LOCATION_MZONE) then
-		Duel.Destroy(tc,REASON_EFFECT)
-	end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local sc=e:GetLabelObject()
+	Duel.Remove(sc,FACE_UP,SEQ_DECKSHUFFLE,REASON_EFFECT)
 end
