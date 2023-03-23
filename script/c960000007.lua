1d9f638df01bc68afacce1d44ffcc24e8633039c
lakevin <KevinLa95@web.de>
2023-03-23 23:27:09 +0100

    Bugfixing / Balancing

--- script/c960000007.lua
+++ script/c960000007.lua
@@ -1,12 +1,12 @@
 -- Abyss of the Chains
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	-- Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	-- (1) remove
+	-- perm remove
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE)
@@ -16,61 +16,105 @@ function s.initial_effect(c)
 	e2:SetTargetRange(0xff,0xff)
 	e2:SetValue(LOCATION_REMOVED)
 	c:RegisterEffect(e2)
-	-- (2) Special Summon
+	-- perm additional normal summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e3:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
 	e3:SetCountLimit(1,id)
-	e3:SetTarget(s.target)
-	e3:SetOperation(s.activate)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x9998))
 	c:RegisterEffect(e3)
-	--extra summon
+	-- (1) Special Summon
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
-	e4:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e4:SetCountLimit(1,id)
-	e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x9998))
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
 	c:RegisterEffect(e4)
-	--
-	--[[local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetCode(id)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e4:SetTargetRange(0xff,0xff)
-	e4:SetTarget(s.checktg)
-	c:RegisterEffect(e4)]]--
-	
+	-- (2) banish + negate effects
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_REMOVE)
+	e5:SetType(EFFECT_TYPE_ACTIVATE)
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetTarget(s.target)
+	e5:SetOperation(s.activate)
+	c:RegisterEffect(e5)
+end
+
+-- perm remove
+function s.rmtarget(e,c)
+	return not c:IsLocation(LOCATION_OVERLAY)
 end
 
 -- (1)
-function s.rmtarget(e,c)
-	return not c:IsLocation(0x80) and not c:IsType(TYPE_SPELL+TYPE_TRAP)
+function s.spfilter(c,e,tp)
+	return c:IsMonster() and c:IsSetCard(0x9999) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.checktg(e,c)
-	return not c:IsPublic()
-end
-
--- (2)
-function s.filter(c,e,tp)
-	return c:IsSetCard(0x9999) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.filter(chkc,e,tp) end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
+end
+
+-- (2)
+function s.filter(c)
+	return c:IsMonster() and c:IsSetCard(0x9999) and not c:IsRitualMonster() and c:IsAbleToRemove() and aux.SpElimFilter(c,true)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_DECK) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 then
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+		e1:SetTarget(s.distg)
+		e1:SetLabel(tc:GetOriginalCodeRule())
+		e1:SetReset(RESET_PHASE+PHASE_END,2)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_CHAIN_SOLVING)
+		e2:SetCondition(s.discon)
+		e2:SetOperation(s.disop)
+		e2:SetLabel(tc:GetOriginalCodeRule())
+		e2:SetReset(RESET_PHASE+PHASE_END,2)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.distg(e,c)
+	local code=e:GetLabel()
+	local code1,code2=c:GetOriginalCodeRule()
+	return code1==code or code2==code
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	local code=e:GetLabel()
+	local code1,code2=re:GetHandler():GetOriginalCodeRule()
+	return re:IsActiveType(TYPE_MONSTER) and (code1==code or code2==code)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.NegateEffect(ev)
 end
