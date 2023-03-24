bdbf53f6cf259c9d7233afad610ec369271ea966
lakevin <KevinLa95@web.de>
2023-03-24 01:36:19 +0100

    Update c960000007.lua

--- script/c960000007.lua
+++ script/c960000007.lua
@@ -80,30 +80,31 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_DECK) and s.filter(chkc,e,tp) end
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,tp,LOCATION_DECK)	
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 then
-		local c=e:GetHandler()
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-		e1:SetTarget(s.distg)
-		e1:SetLabel(tc:GetOriginalCodeRule())
-		e1:SetReset(RESET_PHASE+PHASE_END,2)
-		Duel.RegisterEffect(e1,tp)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e2:SetCode(EVENT_CHAIN_SOLVING)
-		e2:SetCondition(s.discon)
-		e2:SetOperation(s.disop)
-		e2:SetLabel(tc:GetOriginalCodeRule())
-		e2:SetReset(RESET_PHASE+PHASE_END,2)
-		Duel.RegisterEffect(e2,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if tc then
+		if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 then
+			local c=e:GetHandler()
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+			e1:SetTarget(s.distg)
+			e1:SetLabel(tc:GetOriginalCodeRule())
+			e1:SetReset(RESET_PHASE+PHASE_END,2)
+			Duel.RegisterEffect(e1,tp)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e2:SetCode(EVENT_CHAIN_SOLVING)
+			e2:SetCondition(s.discon)
+			e2:SetOperation(s.disop)
+			e2:SetLabel(tc:GetOriginalCodeRule())
+			e2:SetReset(RESET_PHASE+PHASE_END,2)
+			Duel.RegisterEffect(e2,tp)
+		end
 	end
 end
 function s.distg(e,c)
