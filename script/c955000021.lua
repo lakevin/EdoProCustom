50db7e1ab95acee31588a006dd0e94a4b9fcdca1
lakevin <KevinLa95@web.de>
2023-03-25 03:03:05 +0100

    Update c955000021.lua

--- script/c955000021.lua
+++ script/c955000021.lua
@@ -5,6 +5,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 	-- ATK+DEF
 	local e2=Effect.CreateEffect(c)
