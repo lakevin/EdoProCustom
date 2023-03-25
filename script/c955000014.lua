482bd5eeb91ab4996fbdc5ce278525f22c4641c8
lakevin <KevinLa95@web.de>
2023-03-25 19:56:15 +0100

    Update c955000014.lua

--- script/c955000014.lua
+++ script/c955000014.lua
@@ -1,24 +1,59 @@
 --Protectrix Grand Operation
 local s,id=GetID()
 function s.initial_effect(c)
-	-- (1) spsummon
+	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 
--- (1)
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+-- Select Option
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return s.thtg(e,tp,eg,ep,ev,re,r,rp,0,chkc) end
+	-- (1) Special Summon 1 "Protectrix" monster from Deck
+	local b1=s.sptg(e,tp,eg,ep,ev,re,r,rp,0)
+	-- (2) Add 1 "Protectrix" monster from Deck to hand
+	local b2=s.thtg(e,tp,eg,ep,ev,re,r,rp,0)
+	if chk==0 then return b1 or b2 end
+	local op=0
+	if b1 and b2 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+	elseif b1 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,0))
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(id,1))+1
+	end
+	if op==0 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		e:SetProperty(0)
+		e:SetOperation(s.spop)
+		s.sptg(e,tp,eg,ep,ev,re,r,rp,1)
+	else
+		e:SetCategory(CATEGORY_TOHAND)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e:SetOperation(s.thop)
+		s.thtg(e,tp,eg,ep,ev,re,r,rp,1)
+	end
 end
+
+-- (1)
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x9990) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
 
 -- (2)
@@ -26,40 +61,11 @@ function s.thfilter(c,e,tp)
 	return c:IsSetCard(0x9990) and c:IsAbleToHand()
 end
 
--- SelectOption
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
-	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 then
-		local op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
-		e:SetLabel(op)
-	else
-		e:SetLabel(1)
-	end
-	if op==0 then
-		if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-	else
-		if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND+CATEGORY_SEARCH,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	end
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	if #g==0 then return end
-	if e:GetLabel()==0 then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		if g:GetCount()>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
-	else 
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
-		if #g>0 then
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g)
-		end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
