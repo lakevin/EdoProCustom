8de7535f7df30fbee9ddd9e213695d76ffc75f04
lakevin <KevinLa95@web.de>
2023-03-29 22:24:48 +0200

    Balance Patch

--- script/c960000007.lua
+++ script/c960000007.lua
@@ -26,14 +26,14 @@ function s.initial_effect(c)
 	e3:SetTarget(s.checktg)
 	c:RegisterEffect(e3)
 	--Extra summon
-	local e4=Effect.CreateEffect(c)
+	--[[local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetRange(LOCATION_SZONE)
 	e4:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
 	e4:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
 	e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x9998))
-	c:RegisterEffect(e4)
+	c:RegisterEffect(e4)]]--
 	-- (1) Special Summon
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,1))
