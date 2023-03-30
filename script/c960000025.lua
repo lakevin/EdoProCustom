79597ddd6efe158bd3a942ebeaad4752127f1b34
lakevin <KevinLa95@web.de>
2023-03-30 23:16:19 +0200

    Update c960000025.lua

--- script/c960000025.lua
+++ script/c960000025.lua
@@ -26,9 +26,6 @@ function s.initial_effect(c)
 end
 
 -- Global
-function s.splimit(e,c)
-	return not c:IsSetCard(0x9999)
-end
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x9998) and c:GetAttack()<=1500 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -73,14 +70,22 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetCode(EFFECT_CANNOT_SUMMON)
 		e1:SetDescription(aux.Stringid(id,1))
 		e1:SetTargetRange(1,0)
 		e1:SetTarget(s.splimit)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		Duel.RegisterEffect(e2,tp)
 	end
 end
+-- normal / special summon limit
+function s.splimit(e,c)
+	return not c:IsSetCard(0x9999)
+end
+-- destroy during end phase
 function s.desfilter(c,fid)
 	return c:GetFlagEffectLabel(id)==fid
 end
