ce2b68b3c2a7fcd4d7a5614113e79dfdcee0be58
lakevin <KevinLa95@web.de>
2023-04-24 01:57:03 +0200

    Update c960000024.lua

--- script/c960000024.lua
+++ script/c960000024.lua
@@ -5,9 +5,10 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	-- (1)
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,{id,1})
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
@@ -32,7 +33,7 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL)
 end
 function s.spfilter(c,e,tp)
-	return c:IsRitualMonster() and c:IsSetCard(0x9999) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsRitualMonster() and c:IsSetCard(0x9999) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(CATEGORY_REMOVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
@@ -45,7 +46,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
 	end
 end
 
