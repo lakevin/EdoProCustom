e19931420707650199d5ef8a2ce7ba5a0e3e0345
lakevin <KevinLa95@web.de>
2023-04-30 00:39:48 +0200

    Balance Patch
    - Fix effect of "Draconier Goldfin"

--- script/c956000016.lua
+++ script/c956000016.lua
@@ -76,16 +76,18 @@ end
 function s.ptop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		-- "Draconier" immun
+		-- Cannot be destroyed by battle or card effects
 		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetDescription(3110)
+		e1:SetDescription(3008)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_IMMUNE_EFFECT)
-		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-		e1:SetValue(saux.tgoval)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		e1:SetOwnerPlayer(tp)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		tc:RegisterEffect(e2)
 	end
 end
 
