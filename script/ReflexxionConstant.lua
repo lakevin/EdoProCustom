29480deba376fde9d54ea2d5cc82a4af46ef6f07
kla <kevinla95@web.de>
2023-05-18 22:47:38 +0200

    Create ReflexxionConstant.lua

---
+++ script/ReflexxionConstant.lua
@@ -0,0 +1,13 @@
+--Reflexxion Constant File
+
+--Archetype code
+
+SET_PROTECTRIX               = 0x9990
+SET_DRACONIER                = 0x9992
+SET_COSMOVERSE               = 0x9995
+SET_CONTRACTOR               = 0x9998
+SET_GRIMM_CHAIN              = 0x9999
+
+--Card Name Variable
+
+CARD_COSMO_QUEEN             = 38999506
