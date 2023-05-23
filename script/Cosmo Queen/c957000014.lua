7b9d9cbb2547188f7c51ea1ce5b9fc86c3f489a6
lakevin <KevinLa95@web.de>
2023-05-24 00:02:50 +0200

    Bugfix

---
+++ script/Cosmo Queen/c957000014.lua
@@ -0,0 +1,65 @@
+--Cosmoverse Dominance
+local s,id=GetID()
+local CARD_COSMO_QUEEN=38999506
+local SET_COSMOVERSE=0x9995
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetCondition(s.actcon)
+	c:RegisterEffect(e1)
+	--prevent special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	c:RegisterEffect(e2)
+	-- (2) self destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EFFECT_SELF_DESTROY)
+	e3:SetCondition(s.tgcon)
+	c:RegisterEffect(e3)
+	-- (3) destroy when leaving the field
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetCondition(s.descon)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={SET_COSMOVERSE}
+s.listed_names={CARD_COSMO_QUEEN}
+
+-- (1)
+function s.actcon(e)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_COSMO_QUEEN),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+
+-- (2)
+function s.tgcon(e)
+	return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,CARD_COSMO_QUEEN),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+
+-- (3)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousPosition(POS_FACEUP) and not c:IsLocation(LOCATION_DECK)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,nil)
+	Duel.Destroy(g,REASON_EFFECT)
+end
