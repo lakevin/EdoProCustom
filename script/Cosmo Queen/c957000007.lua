2f139745ef4445f726d90393786671a0b3beeb48
lakevin <KevinLa95@web.de>
2023-05-23 19:20:46 +0200

    Update c957000007.lua

--- script/Cosmo Queen/c957000007.lua
+++ script/Cosmo Queen/c957000007.lua
@@ -47,72 +47,29 @@ function s.cfilter(c)
 	return c:IsCode(CARD_COSMO_QUEEN) and c:IsType(TYPE_RITUAL|TYPE_FUSION|TYPE_SYNCHRO|TYPE_XYZ|TYPE_LINK)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
-	Duel.SetTargetParam(Duel.SelectOption(tp,1057,1056,1063,1073,1074))
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local opt=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-	local ct=nil
-	if opt==0 then ct=TYPE_RITUAL end
-	if opt==1 then ct=TYPE_FUSION end
-	if opt==2 then ct=TYPE_SYNCHRO end
-	if opt==3 then ct=TYPE_XYZ end
-	if opt==4 then ct=TYPE_PENDULUM end
-	--Cannot Special Summon monsters of the declared type
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,5))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetTargetRange(1,1)
-	e1:SetTarget(s.sumlimit)
-	e1:SetLabel(ct)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	--Negate the effects of monsters of that type while on the field
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_DISABLE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(function(e,c) return c:IsType(e:GetLabel()) end)
-	e2:SetLabel(ct)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-end
-function s.sumlimit(e,c,sump,sumtype,sumpos,targetp)
-	local declared_type=e:GetLabel()
-	if c:IsMonster() then
-		return c:IsType(declared_type)
-	else
-		return c:IsOriginalType(declared_type)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil)
 	end
-end
---[[function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	--if chk==0 then
-	--	if e:GetLabel()~=100 then return false end
-	--	e:SetLabel(0)
-	--	return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil)
-	--end
 	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil)
 	e:SetLabel(g:GetFirst():GetType())
 	Duel.Release(g,REASON_COST)
-end]]--
---[[function s.operation(e,tp,eg,ep,ev,re,r,rp)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local ct=e:GetLabel()
 	--Cannot Special Summon monsters of the declared type
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,5))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetTargetRange(1,1)
-	e1:SetTarget(s.sumlimit)
-	e1:SetLabel(ct)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
+	--local e1=Effect.CreateEffect(c)
+	--e1:SetDescription(aux.Stringid(id,5))
+	--e1:SetType(EFFECT_TYPE_FIELD)
+	--e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	--e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	--e1:SetTargetRange(1,1)
+	--e1:SetTarget(s.sumlimit)
+	--e1:SetLabel(ct)
+	--e1:SetReset(RESET_PHASE+PHASE_END)
+	--Duel.RegisterEffect(e1,tp)
 	--Negate the effects of monsters of that type while on the field
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
@@ -130,4 +87,4 @@ function s.sumlimit(e,c,sump,sumtype,sumpos,targetp)
 	else
 		return c:IsOriginalType(declared_type)
 	end
-end]]--
+end
