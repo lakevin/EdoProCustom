3d2df765725cb35b03538785f139bd0fb033b1f5
lakevin <KevinLa95@web.de>
2023-06-25 22:10:47 +0200

    Update c85679527.lua

--- script/Cosmo Queen/c85679527.lua
+++ script/Cosmo Queen/c85679527.lua
@@ -25,7 +25,26 @@ function s.initial_effect(c)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-function s.spfilter1(c,tp)
+
+-- (1)
+function s.cfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsDiscardable() and c:IsAbleToGraveAsCost()
+end
+function s.spcost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST+REASON_DISCARD,e:GetHandler())
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)
+end
+--[[function s.spfilter1(c,tp)
 	return (c:IsLocation(LOCATION_HAND) or c:IsFaceup()) and not c:IsType(TYPE_EFFECT)
 		and c:IsMonster() and c:IsAbleToGraveAsCost()
 end
@@ -53,7 +72,9 @@ function s.spop1(e,tp,eg,ep,ev,re,r,rp,c)
 	e1:SetValue(lv*200)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_DISABLE)
 	e:GetHandler():RegisterEffect(e1)
-end
+end]]--
+
+-- (2)
 function s.costfilter(c,tp)
 	return c:IsType(TYPE_EFFECT) and Duel.GetMZoneCount(tp,c,tp)>0
 end
