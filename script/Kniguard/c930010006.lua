5eaae403e5fdad8ec2eeaa6045b24c0a8b202dc9
lakevin <KevinLa95@web.de>
2023-11-07 02:28:50 +0100

    Fixing hybrid archetype lock

--- script/Kniguard/c930010006.lua
+++ script/Kniguard/c930010006.lua
@@ -5,6 +5,14 @@ local COUNTER_GRAIL=0x4041
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableCounterPermit(COUNTER_GRAIL)
+	-- attribute
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e0:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e0:SetValue(ATTRIBUTE_LIGHT)
+	c:RegisterEffect(e0)
 	-- (1) Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
